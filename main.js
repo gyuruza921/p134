30d357ad4fc6c7cc11555ca5f02516dab706969b
gyurza921 <objekt.172.uru@gmail.com>
2020-08-02 12:28:53 +0900

    canvas要素の追加

---
+++ main.js
@@ -0,0 +1,36 @@
+
+"use strict"
+
+// 
+// DOM構成部
+//
+
+// canvas要素の作成
+const canvas0 = document.createElement("canvas");
+canvas0.id = "canvas0";
+canvas0.height = 500;
+canvas0.width = 500;
+
+// 要素のドキュメントへの追加
+document.appendChild(canvas0);
+
+// 
+// 入力部
+// 
+
+
+// 
+// 処理部
+// 
+
+
+// 
+// 表示部
+// 
+
+
+// 
+// 操作部
+// 
+
+
