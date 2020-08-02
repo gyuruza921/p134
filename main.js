df200005e8fb586b402190a240b035227f38088a
gyuruza921 <68643520+gyuruza921@users.noreply.github.com>
2020-08-02 13:49:37 +0900

    ドキュメント構成部の修正

--- main.js
+++ main.js
@@ -5,6 +5,9 @@
 // DOM構成部
 //
 
+// body要素の取得
+const BODY = document.getElementByTagName("body");
+
 // canvas要素の作成
 const canvas0 = document.createElement("canvas");
 canvas0.id = "canvas0";
@@ -12,7 +15,7 @@ canvas0.height = 500;
 canvas0.width = 500;
 
 // 要素のドキュメントへの追加
-document.body.appendChild(canvas0);
+BODY.appendChild(canvas0);
 
 // 
 // 入力部
