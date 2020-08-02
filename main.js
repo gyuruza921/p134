9fab3339763acb69c5727d872755e0bdd1e4c34f
gyurza921 <objekt.172.uru@gmail.com>
2020-08-02 13:55:10 +0900

    main.jsの修正

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 //
 
 // body要素の取得
-const BODY = document.getElementByTagName("body");
+const BODY = document.getElementsByTagName("body");
 
 // canvas要素の作成
 const canvas0 = document.createElement("canvas");
