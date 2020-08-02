2d2d458802c7cd4ef13a908dc107fc7ff8c42677
gyurza921 <objekt.172.uru@gmail.com>
2020-08-02 14:12:49 +0900

    修正

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 //
 
 // body要素の取得
-const BODY = document.getElementsByTagName("body");
+const BODY = document.getElementId("body");
 
 // canvas要素の作成
 const canvas0 = document.createElement("canvas");
